3a38979fbbf242b95b17e683843be4b533754f1f
Chifurumnanya! <c.alex-uzoatu@stu.unizik.edu.ng>
2022-09-15 14:38:39 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,28 +1,44 @@
-#include <stdio.h> 
-  
- /** 
-  * main - prints numbers from 1 to 100 followed by a new line and replaces 
-  * Fizz, Buzz or FizzBuzz if the number is a multiple of 3, 5 
-  * 
-  * Return: 0 
-  */ 
- int main(void) 
- { 
-         int a = 1, i = 2; 
-  
-         printf("%d", a); 
-         while (i < 101) 
-         { 
-                 if (i % 3 == 0 && i % 5 == 0) 
-                         printf(" FizzBuzz"); 
-                 else if (i % 3 == 0) 
-                         printf(" Fizz"); 
-                 else if (i % 5 == 0) 
-                         printf(" Buzz"); 
-                 else 
-                         printf(" %d", i); 
-                 i++; 
-         } 
-         printf("\n"); 
-         return (0); 
- }
+#include <stdio.h>
+
+/**
+*main - program that prints either number
+*or fizz or buzz or fizzBuzz
+*
+*
+*Return: returns 0
+*/
+
+int main(void)
+{
+	int num = 1;
+
+	while (num++ < 100)
+	{
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((num % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((num % 5) == 0)
+		{
+			if (num != 100)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz");
+			}
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
